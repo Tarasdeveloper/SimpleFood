5bfc80ec35f3ad60c6f341db95d4830d2668af59
Tarasdeveloper <tomahavki1@gmail.com>
2022-07-23 23:31:04 +0300

    added favicons

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function(){
 
-
-
-
+   $('.review__slider').slick({
+      dots: true,
+      prevArrow: '<span class="review__arrow review__arrow-left"><svg><use xlink:href="images/sprite.svg#prevarrow"></use></svg>',
+      nextArrow: '<span class="review__arrow review__arrow-right"><svg><use xlink:href="images/sprite.svg#nextarrow"></use></svg>'
+   });
 
 
    var mixer = mixitup('.categories__meals');
