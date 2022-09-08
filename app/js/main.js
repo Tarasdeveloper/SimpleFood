4429e1d0f6fda456d698bb645246ab312157182f
Tarasdeveloper <tomahavki1@gmail.com>
2022-09-08 22:18:29 +0300

    adaptive added

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,27 @@ $(function(){
       nextArrow: '<span class="review__arrow review__arrow-right"><svg><use xlink:href="images/sprite.svg#nextarrow"></use></svg>'
    });
 
-
    var mixer = mixitup('.categories__meals');
+
+   const mediaQuery = window.matchMedia('(max-width: 576px)')
+
+   function handleTabletChange(e) {
+      if (e.matches) {
+         $('.restaurants__inner').slick({
+            arrows: false,
+            dots: true
+         });
+      } else {
+         false
+      }
+    }
+    mediaQuery.addListener(handleTabletChange)
+    handleTabletChange(mediaQuery)
+
+
+    $('.menu-btn, .menu-btnclose').on('click', function() {
+      $('.menu-btn,.menu__mobile').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
 
 });
